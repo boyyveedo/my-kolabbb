904353362a9399a7fa432a4136549b62b7e039a4
boyyveedo <talktoboyveedo@gmail.com>
2024-12-07 21:08:32 +0100

    database

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
+const logger = require('../logger/index')
+require('dotenv').config();
 
-//connecting to database
-const connectDB = (url) => {
-    return mongoose.connect(url)
-}
+// Function to connect to MongoDB
+const connectDB = async () => {
+    try {
+        // Get the MongoDB URI from the environment variables
+        const mongoURI = process.env.MONGO_URI;
 
+        // Check if the URI exists in the environment variables
+        if (!mongoURI) {
+            logger.error("MongoDB URI is not defined in environment variables.");
+            process.exit(1);  // Exit if the URI is not found
+        }
 
+        // Connect to the MongoDB database using the URI
+        await mongoose.connect(mongoURI, {
 
+        });
 
+        logger.info('MongoDB connected successfully');
+    } catch (err) {
+        logger.error('Error connecting to MongoDB:', err.message);
+        process.exit(1);  // Exit the process if the connection fails
+    }
+};
 
-
-module.exports = connectDB
-
-
+module.exports = connectDB;
