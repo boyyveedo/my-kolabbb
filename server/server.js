01b52aa4ed62d40117d4647d62a4156de81500cd
boyyveedo <talktoboyveedo@gmail.com>
2024-12-07 23:04:42 +0100

    cors updated

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(helmet());
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://ofensala.dev',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
 }));
