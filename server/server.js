77e57935e1cb1844b0b0fb05b6f91ff3710a5ce3
boyyveedo <talktoboyveedo@gmail.com>
2024-12-09 00:08:29 +0100

    api updated

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(helmet());
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://kolabbb.com',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
 }));
